9342803827468dfab745312e099123490c631154
bailec <bailec@rpi.edu>
2008-10-09 04:27:55 +0000

    PHP files with the HTML for all the pages I started, more to come later

---
+++ artist_alerter/website_templates/index.php
@@ -0,0 +1,63 @@
+
+<html><head>
+<title>Artist Alert - SD&D</title><link href="default.css" rel="stylesheet" type="text/css"></head>
+<body>
+
+<div style="height: 100px;" id="logo">
+	<img src="images/banner.jpg">
+</div>
+
+<div id="content">
+	<div id="sidebar">
+		<div id="menu">
+			<ul>
+				<li class="active"><a href="index.php">Home</a></li>
+				<li><a href="download.php">Download</a></li>
+				<li><a href="library.php">Library</a></li>
+				<li><a href="about.php">About Us</a></li>
+			</ul>
+		</div>
+		<div id="login" class="boxed">
+			<h2 class="title">MyLibrary</h2>
+			<div class="content">
+				<form>
+					<fieldset>
+					<label for="inputtext1">User Name:</label>
+					<input id="inputtext1" name="inputtext1" type="text">
+					<label for="inputtext2">Password:</label>
+					<input id="inputtext2" name="inputtext2" type="password">
+					<input id="inputsubmit1" name="inputsubmit1" value="Sign In" type="submit"><br/>
+					<a href="register.php">Need an account?</a>
+					<p>
+					</p></fieldset>
+				</form>
+			</div>
+		</div>
+		<div id="updates" class="boxed">
+			<h2 class="title">Recent Updates</h2>
+			<div class="content">
+				<ul>
+					<li>
+						<h3>October 5, 2008</h3>
+						<p><a href="index.php">First iteration of website</a></p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div id="main">
+		<div id="welcome" class="post">
+			<h2 class="title">Your Music Source</h2>*Insert VISION STATEMENT here* 
+			
+		</div>
+		
+		
+	</div>
+</div>
+
+<div id="footer">
+	<p style="margin-left: 0px; width: 800px;" id="legal">Team Victorious Secret<br/>2008 SD&D
+</p></div>
+
+
+</body></html>
