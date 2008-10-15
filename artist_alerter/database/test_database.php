9d1eb28016f07905d2db0b548f27491971183ae6
awaters1 <awaters1@b2ae0a10-7dff-11dd-8f23-d9c8888c2974>
2008-10-15 20:17:40 +0000

    fixed the database sql generation added sample code for doctrine handling

--- artist_alerter/database/test_database.php
+++ artist_alerter/database/test_database.php
@@ -2,6 +2,20 @@
 require_once('config.php');
 	
 	$conn = Doctrine_Manager :: connection(DSN);
+	
+	
+	$user = new User();
+	
+	//$user['first_name'] = 'test';
+	$user['last_name'] = 'test2';
+	$user['password'] = md5('fun');
+	$user['email_address'] = 'test4';
+	
+	try {
+		$user->save();
+	} catch (Doctrine_Connection_Pgsql_Exception $e) {
+		print $e;
+	} 
 
 	$artists = Doctrine_Query::create()
 		          ->from('Artist a')
