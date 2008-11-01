534e2f846a919d8dd9e4c400192ab93de7828368
awaters1 <awaters1@b2ae0a10-7dff-11dd-8f23-d9c8888c2974>
2008-11-01 20:59:50 +0000

    update send/view recommendations to be styled like everything else added a logout link

--- artist_alerter/website_templates/sidebar.php
+++ artist_alerter/website_templates/sidebar.php
@@ -16,7 +16,9 @@
 				<li class="<?php if (strpos($_SERVER['PHP_SELF'],'about.php') != false) print('active');?>"><a href="about.php">About Us</a></li>
 				<?php if ($_SESSION['user']) { ?>
 					<!-- This stuff shows up when the user is logged in -->
-					 <li class="<?php if (strpos($_SERVER['PHP_SELF'],'send_recommendation.php') != false) print('active');?>"><a href="recommendations/send_recommendation.php">Send Recommendation</a></li>
+					 <li class="<?php if (strpos($_SERVER['PHP_SELF'],'send_recommendation.php') != false) print('active');?>"><a href="send_recommendation.php">Send Recommendations</a></li>
+					 <li class="<?php if (strpos($_SERVER['PHP_SELF'],'view_recommendations.php') != false) print('active');?>"><a href="view_recommendations.php">View Recommendations</a></li>
+					 <li><a href="index.php?logout=1">Logout</a></li>
 				<?php } ?>
 			</ul>
 		</div>
