162ff362e0ac77fa2ad95bc50b0df46fa569b4a2
prismhdd <prismhdd@b2ae0a10-7dff-11dd-8f23-d9c8888c2974>
2008-11-12 05:46:25 +0000

    parsing done. need to add database part

--- trunk/artist_alerter/website_templates/library.php
+++ trunk/artist_alerter/website_templates/library.php
@@ -5,7 +5,6 @@
 <body>
 
 <?php require_once('header.php'); ?>
-
 <div id="content">
 	<?php require_once('sidebar.php') ?>
 	<div id="main">
@@ -16,30 +15,74 @@
 			<input type="file" name="file">
 			<input type="submit" value="Upload">
 			<?php
+			// database part need to be added
 				if($_POST[upload] == "1")
 				{
 					$file = $_FILES['file']['tmp_name'];
 					
-					// function to handle the start tags 
-					function startTag($parser, $data){
-					    //echo "<p>";
-					}
+					//function starts					
+					$current_tag = '';
+					$current_data = '';
+					$current_artist = '';
+					//bug for data handler
+					//keep counts will help to match start_handle = char_handle
+					$s_count = 0;
+					$d_count = 0;
+					$artists = array();
 					
-					// function to handle the data between the tags 
-					function contents($parser, $data){
-					    //echo $data;
+					// function to handle the start tags 
+					echo "<br>";
+					function start_element_handler($parser, $name, $attributes){
+					    global $current_tag;
+					    global $current_data;
+						global $current_artist;
+						global $artists;
+						global $s_count;
+						global $d_count;
+					    
+					    $s_count += 1;
+					    $current_tag = $name;
 					}
 					
 					// function to handle the end tags 
-					function endTag($parser, $data){
-					    //echo "</p><br>";
+					function end_element_handler($parser, $name){
 					}
-					
-					$xml_parser = xml_parser_create();
-					
-					xml_set_element_handler($xml_parser, "startTag", "endTag");
-					
-					xml_set_character_data_handler($xml_parser, "contents");
+								
+					// function to handle the data between the tags 
+					function character_data_handler($parser, $data){
+						global $current_tag;
+					    global $current_data;
+						global $current_artist;
+						global $artists;
+						global $s_count;
+						global $d_count;
+						
+						$d_count += 1;
+//						echo "s_count : " . $s_count . "<br>";
+//						echo "d_count : " . $d_count . "<br>";
+						if ($s_count == $d_count) {
+							//echo "data : " . $data . "<br>";
+						    //$current_data = $data;
+							if ($current_tag == 'ARTIST')
+						    {
+						    	$current_artist = $data;
+						    	//echo "current artist : " . $current_artist . "<br>";
+						    }
+						    if ($current_tag == 'ALBUM') {
+						    	$current_data = $data;
+						    	//echo "current artist : " . $current_artist . "<br>";
+						    	//echo "current data : "	. $current_data . "<br>";				    	
+						    	if (!array_key_exists($current_artist, $artists)) {
+									$artists[$current_artist] = array ();
+								}
+								//echo $current_data . " added to " . $current_artist . " key<br>"; 
+								array_push($artists[$current_artist], $current_data);
+							}
+						}
+						else {
+							$d_count -= 1;
+						}
+					}
 					
 					if (!($fp = fopen($file, "r"))) {
 					    die("could not open XML input");
@@ -47,22 +90,25 @@
 					
 					$data = fread($fp, filesize($file));
 					
+					$xml_parser = xml_parser_create();
+					xml_set_object($xml_parser, $file);
+					xml_set_element_handler($xml_parser, "start_element_handler", "end_element_handler");
+					xml_set_character_data_handler($xml_parser, "character_data_handler");
+					
+					// print out
 					if(!(xml_parse($xml_parser, $data))){
 					    die("Error on line " . xml_get_current_line_number($xml_parser));
 					}
-					
+					else {
+						foreach ($artists as $artist => $albums) {
+							echo "Artist: $artist<br>";
+							foreach ($albums as $album) {
+								echo "&nbsp;&nbsp;&nbsp;Album: $album<br />\n";
+							}
+						}
+					}
 					xml_parser_free($xml_parser);
-					
 					fclose($fp);
-					
-					echo "<br>" . $data;
-					
-					/*
-					$data = fread($fp, filesize($file));
-					fclose($fp);
-					xml_parse_into_struct($xml_parser, $data, $vals, $index);
-					xml_parser_free($xml_parser);
-					*/
 				}
 			?>
 			
