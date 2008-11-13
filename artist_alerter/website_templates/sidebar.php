7b74b12c38e133f2d4b7af4ebfa59ab3bb100adf
awaters1 <awaters1@b2ae0a10-7dff-11dd-8f23-d9c8888c2974>
2008-11-13 01:01:53 +0000

    updated send_notifications to actually send out notifications to theusers email address updated the side bar to only show my library when the user is logged in

--- artist_alerter/website_templates/sidebar.php
+++ artist_alerter/website_templates/sidebar.php
@@ -12,10 +12,10 @@
 			<ul>
 				<li class="<?php if (strpos($_SERVER['PHP_SELF'],'index.php') != false) print('active');?>"><a href="index.php">Home</a></li>
 				<li class="<?php if (strpos($_SERVER['PHP_SELF'],'download.php') != false) print('active');?>"><a href="download.php">Download</a></li>
-				<li class="<?php if (strpos($_SERVER['PHP_SELF'],'library.php') != false) print('active');?>"><a href="library.php">Library</a></li>
 				<li class="<?php if (strpos($_SERVER['PHP_SELF'],'about.php') != false) print('active');?>"><a href="about.php">About Us</a></li>
 				<?php if ($_SESSION['user']) { ?>
 					<!-- This stuff shows up when the user is logged in -->
+					<li class="<?php if (strpos($_SERVER['PHP_SELF'],'library.php') != false) print('active');?>"><a href="library.php">Library</a></li>
 					 <li class="<?php if (strpos($_SERVER['PHP_SELF'],'send_recommendation.php') != false) print('active');?>"><a href="send_recommendation.php">Send Recommendations</a></li>
 					 <li class="<?php if (strpos($_SERVER['PHP_SELF'],'view_recommendations.php') != false) print('active');?>"><a href="view_recommendations.php">View Recommendations</a></li>
 					 <li class="<?php if (strpos($_SERVER['PHP_SELF'],'view_user_info.php') != false) print('active');?>"><a href="view_user_info.php">User Info</a></li>					 
