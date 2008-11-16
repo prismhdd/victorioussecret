9e3065627af63c2068265adc7cbdf6d076d78ede
bailec@rpi.edu <bailec@rpi.edu@b2ae0a10-7dff-11dd-8f23-d9c8888c2974>
2008-11-16 05:04:11 +0000

    Minor stuff - added a sidebar update just for completeness, changed a margin in the menu styling so the text isn't crowding the edges.

--- trunk/artist_alerter/website_templates/updates.php
+++ trunk/artist_alerter/website_templates/updates.php
@@ -3,6 +3,10 @@
 	<div class="content">
 		<ul>
 			<li>
+				<h3>November 14, 2008</h3>
+				<p><a href="index.php">Website goes live for beta release</a></p>
+			</li>
+			<li>
 				<h3>October 23, 2008</h3>
 				<p><a href="index.php">User registration and login are in place</a></p>
 			</li>				
